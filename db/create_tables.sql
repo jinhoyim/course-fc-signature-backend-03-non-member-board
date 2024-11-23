70a6493f06c5592f0d46661d89ed679eea190c00
Jinho Yim <jinhoyim@outlook.com>
2024-11-23 12:28:49 +0900

    테이블 생성

---
+++ db/create_tables.sql
@@ -0,0 +1,33 @@
+create table board(
+    id bigint not null auto_increment,
+    board_name varchar(50) not null,
+    status varchar(50) not null,
+    primary key (id)
+) engine innodb character set utf8mb4;
+
+create table post(
+    id bigint not null auto_increment,
+    board_id bigint not null,
+    username varchar(50) not null,
+    password varchar(128) not null,
+    email varchar(128),
+    status varchar(50) not null,
+    title varchar(100) not null,
+    content text not null,
+    posted_at datetime not null,
+    primary key (id)
+) engine innodb character set utf8mb4;
+
+create table reply(
+    id bigint not null auto_increment,
+    post_id bigint not null,
+    username varchar(50) not null,
+    password varchar(128) not null,
+    status varchar(50) not null,
+    title varchar(100) not null,
+    content text not null,
+    replied_at datetime not null,
+    primary key (id)
+) engine innodb character set utf8mb4;
+
+# FK 생략
