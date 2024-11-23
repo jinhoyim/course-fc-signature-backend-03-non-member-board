5b2a8dfb496360cfcf4b84ee2c20b22d1b09da08
Jinho Yim <jinhoyim@outlook.com>
2024-11-23 11:28:41 +0900

    Init

---
+++ src/main/java/course/fastcampus/signature_backend_path/simpleboard/SimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package course.fastcampus.signature_backend_path.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/course/fastcampus/signature_backend_path/simpleboard/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package course.fastcampus.signature_backend_path.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
